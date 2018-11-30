b5e84a99327cff28c816e4b29c6fc0ebe9d19d57
hvonhagk <44850028+hvonhagk@users.noreply.github.com>
2018-11-30 16:03:35 -0500

    Add files via upload

---
+++ news.php
@@ -0,0 +1,21 @@
+<?php
+include ('top.php');
+?>
+        <article class=''>
+            <h1><a href="">INSERT CAPTION</a></h1>
+            
+            <figure>
+                <img src="" alt="">
+                <figcaption>Album name</figcaption>
+            </figure>
+            <p>Reviews of the different albums......</p>
+
+            
+        </article>
+
+ 
+        <?php
+include ('footer.php')
+?>
+    </body>
+</html>
