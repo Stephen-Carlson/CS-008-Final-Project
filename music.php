3e5aa25e91885a68fd030a7bee554bde8b2151a6
HotSweatyMen <44876257+HotSweatyMen@users.noreply.github.com>
2018-12-02 19:26:32 -0500

    Add files via upload

--- music.php
+++ music.php
@@ -1,22 +1,46 @@
 <?php
-include 'top.php'
+include ('top.php');
 ?>
 
+<h1>Our Albums</h1>
 
+<article class="container">
+    <img src="../album_pics/naptime.png" alt="test" class="image" style="width:100%">
+    <figure class="middle">
+        <a href="Album1.php" class="text">Nap Time, 2018</a>
+    </figure>
+</article>
 
+<article class="container" >
+    <img src="../album_pics/album2.png" alt="test" class="image" style="width:100%" >
+    <figure class="middle" >
+        <a href="Album2.php" class="text">Light and Stuff, 2018</a>
+    </figure>
+</article>
 
+<article class="container">
+    <img src="../album_pics/album3.png" alt="test" class="image" style="width:100%">
+    <figure class="middle">
+        <figcaption class="text">Books, 2018</figcaption>
+    </figure>
+</article>
 
+<article class="container">
+    <img src="../album_pics/album4.png" alt="test" class="image" style="width:100%">
+    <figure class="middle">
+        <figcaption class="text">Bottom Text, 2018</figcaption>
+    </figure>
+</article>
 
-
-
-
-
-
-
-
-
+<article class="container">
+    <img src="../album_pics/album5.png" alt="test" class="image" style="width:100%">
+    <figure class="middle">
+        <figcaption class="text">Fruit, 2018</figcaption>
+    </figure>
+</article>
 
 <?php
-include 'footer.php'
+include ('footer.php');
 ?>
-
+    </body>
+</html>
